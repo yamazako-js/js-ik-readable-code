aa0f6a8dbbfccfed53c80ca892cbf86e5215c84a
ishihark <kenji.ishihara@justsystems.com>
2019-09-24 13:34:57 +0900

    spec3: ユーザが任意に作成したファイルを読み込めるように引数で辞書ファイル名を指定できるように変更

--- src/main/java/com/justsystems/readablecode/Main.java
+++ src/main/java/com/justsystems/readablecode/Main.java
@@ -4,12 +4,25 @@ package com.justsystems.readablecode;
 
 public class Main {
 
-    private void printHeading() {
-        ResourceUtil.readWordsInfo("data/dictionary-data.tsv").forEach(System.out::println);
+    /**
+     * 辞書情報を読み込み、標準出力に表示する。
+     * ファイルは`data`ディレクトリに配置されている想定となっている。
+     *
+     * @param fileName 読み込む辞書ファイル名
+     */
+    private void printHeading(String fileName) {
+        String filePath = String.join("/", "data", fileName);
+        ResourceUtil.readWordsInfo(filePath).forEach(System.out::println);
     }
 
+
+
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("プログラムの実行には、引数に辞書ファイル名を一つだけ指定する必要があります。");
+            System.exit(1);
+        }
         Main main = new Main();
-        main.printHeading();
+        main.printHeading(args[0]);
     }
 }
