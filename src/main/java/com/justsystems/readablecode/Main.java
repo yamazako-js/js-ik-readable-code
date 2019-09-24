639451a627f39c0673b5d47dd91ea8b7c93c0758
ishihark <kenji.ishihara@justsystems.com>
2019-09-24 11:26:47 +0900

    spec1: 見出しを標準出力で出力する処理を作成

---
+++ src/main/java/com/justsystems/readablecode/Main.java
@@ -0,0 +1,12 @@
+package com.justsystems.readablecode;
+
+public class Main {
+
+    private static void printHeading() {
+        System.out.println("上手");
+    }
+
+    public static void main(String[] args) {
+        printHeading();
+    }
+}
